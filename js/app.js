4b997e3563c687d62576a234d19574891ccc3fa9
Yashique <yashiquechalil@gmail.com>
2024-12-10 20:10:08 +0000

    rnbo setup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 async function setup() {
-    const patchExportURL = "export/patcher.export.json";
+    const patchExportURL = "export/patch.export.json";
 
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
